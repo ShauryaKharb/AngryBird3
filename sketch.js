c5f2bb6873e274f0d1b13b5f5d123af02622b715
ShauryaKharb <228geetakharb@gmail.com>
2020-11-18 15:51:36 +0530

    Change

--- sketch.js
+++ sketch.js
@@ -1,10 +1,12 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var constrainedLog;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -26,15 +28,18 @@ function setup(){
 
     box3 = new Box(700,240,70,70);
     box4 = new Box(920,240,70,70);
-    pig3 = new Pig(810, 220);
+    pig2 = new Pig(810, 220);
 
     log3 =  new Log(810,180,300, PI/2);
 
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
-
+    constrainedLog = new Log(230,180,80, PI/2);
     bird = new Bird(100,100);
+
+    chain = new Chain(bird.body,pig1.body);
+    chain1 = new Chain(bird.body,pig2.body);
 
 }
 
@@ -52,13 +57,19 @@ function draw(){
 
     box3.display();
     box4.display();
-    pig3.display();
+    pig2.display();
     log3.display();
 
     box5.display();
     log4.display();
     log5.display();
 
+    constrainedLog.display();
+
     bird.display();
     platform.display();
+
+    chain.display();
+    chain1.display();
+    //line(bird.body.position.x, bird.body.position.y,constrainedLog.body.position.x,constrainedLog.body.position.y);
 }
